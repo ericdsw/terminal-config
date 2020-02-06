e80cb725fa2d481ec541a63fc09fbc4554ef40fd
Eric De Sedas <eric.desedas.w@gmail.com>
2020-02-05 19:33:25 -0500

    Add goto php alias, fix install.sh

--- zsh-custom/aliases.zsh
+++ zsh-custom/aliases.zsh
@@ -4,6 +4,7 @@ alias goto:projects="cd ~/Development/projects"
 alias goto:godot="cd ~/Development/projects/godot"
 alias goto:android="cd ~/Development/projects/android"
 alias goto:javascript="cd ~/Development/projects/javascript"
+alias goto:php="cd ~/Development/projects/php"
 
 alias goto:configurations="cd ~/Development/configuration"
 
